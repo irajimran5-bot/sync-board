610aa1877849283f9d76effc3a6d37727c427a2e
Iraj Imran <irajimran5@gmail.com>
2026-07-05 02:48:22 -0700

    Edited server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.get('/',(req,res)=>{
 });
 app.use('/api/boards',require('./routes/boardRoutes'));
 app.use('/api/lists',require('./routes/listRoutes'));
-
+app.use('/api/cards',require('./routes/cardRoutes'));
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
     console.log(`Running on port: ${PORT}`);
