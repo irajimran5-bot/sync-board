6d563f6eff25e4f476257dddc3c8b9db18deb11a
Iraj Imran <irajimran5@gmail.com>
2026-07-05 02:40:23 -0700

    Added Cards

---
+++ backend/controllers/cardController.js
@@ -0,0 +1,25 @@
+const Card=require('../models/Card');
+const List=require('../models/List');
+
+exports.createCard=async(req,res)=>{
+    try{
+        const {title,description,listId,label}=res.body;
+        if(!title||!listId){
+            return res.status(400).json({message:'Title and list ID are required'});
+        }   
+            const parentList=await List.findById(listId);
+            if(!parentList){
+                return res.status(404).json({message:'Column not found'});
+
+            }
+            const newCard=await Card.create({title,description,listId,label});
+            parentList.cards.push(newCard._id);
+            await parentList.save();
+            res.status(201).json(newCard);
+
+        
+    }catch(error){
+        res.status(500).json({message:'Server ERROR',error:error.message});
+
+    }
+};
