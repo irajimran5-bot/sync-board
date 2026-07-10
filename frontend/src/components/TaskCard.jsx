ebb3fd2f73575f9e7c251efb475c11288503c7b9
Iraj Imran <irajimran5@gmail.com>
2026-07-09 23:02:58 -0700

    Made the card draggable

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -6,8 +6,20 @@ function TaskCard({card,onDeleteClick}){
     Medium:"bg-amber-500/10 text-amber-400 border-amber-500/20",
     Low: "bg-emerald-500/10 text-emerald-400 border-emerald-500/20"
   };
+  const handleDragStart=(e)=>{
+    console.log("Drag started for card ID",card._id);
+    e.dataTransfer.setData("text/plain",card._id);
+    e.dataTransfer.setData("sourceListId",card.listId);
+    e.dataTransfer.effectAllowed="move";
+
+  };
+
   return(
-    <div className="group relative bg-slate-900/90 border border-slate-700/50 hover:border-slate-600 p-3 rounded-lg shadow-xs hover:shadow-md transition-all duration-200">
+
+    <div 
+     draggable="true"
+     onDragStart={handleDragStart}
+     className="group relative bg-slate-900/90 border border-slate-700/50 hover:border-slate-600 p-3 rounded-lg shadow-xs hover:shadow-md transition-all duration-200">
       <div className="flex justify-between items-center gap-2 mb-2">
         <span className={`text-[10px] px-2 py-0.5 rounded-md border font-semibold tracking-wider uppercase ${labelColors[card.label] || "bg-slate-700 text-slate-300"}`}>
           {card.label}
